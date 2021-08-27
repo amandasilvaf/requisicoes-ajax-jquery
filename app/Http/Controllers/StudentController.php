7a0695b524d9573492e0825946fa14927be2bbd6
Amanda Ferreira <amandasilvaf1995@gmail.com>
2021-08-27 11:16:56 -0300

    Cria metodo index() do StudentController
    E sua respectiva rota e view

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -3,8 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Student;
 
 class StudentController extends Controller
 {
-    //
+    public function index()
+    {
+        $students = Student::orderBy('id')->get();
+        return view('students', compact('students'));
+    }
 }
